3479895f7ff36b1b5222c000dc75bc631fee91f9
TheresePlaton <s929511@hioa.no>
2015-04-15 18:42:40 +0200

    Regnebelop
    regler etc og metode for å beregne skadebeløp ved totalskadee

--- Forsikringstest/src/forsikringstest/RegneBelop.java
+++ Forsikringstest/src/forsikringstest/RegneBelop.java
@@ -12,8 +12,34 @@ package forsikringstest;
 public interface RegneBelop {
     
     class Utbetaling{
-        
+      // beregner sum til utbetaling, dersom boligen er totalskadet, -1 hvis metoden ikke fører til noe beløp.
+      public double fullUtbetaling( double skadeBelop , double byggVerdi, int alder ){
+          // regler for utbetaling, summen som betales ut avhenger av disse reglene.
+          double totalSkadeGrense = 0.75;
+          double aldersFradrag10Ar = 0.015;
+          double aldersFradrag5Ar = 0.01;
+          
+          double minTotal= byggVerdi* totalSkadeGrense;
+          if ( minTotal <= skadeBelop  && skadeBelop<= byggVerdi ){
+            double sum;
+               if(alder>= 10){
+                    sum = byggVerdi - byggVerdi*alder*aldersFradrag10Ar;
+                        return sum;
+                }
+                else if(alder>= 5){
+                    sum = byggVerdi - byggVerdi*alder*aldersFradrag5Ar;
+                        return sum;
+                }
+                else if(alder<5){
+                   sum = byggVerdi;
+                   return sum;
+                }
+               
+          }
+          return -1;
+      }// end of method fullUtbetaling
     }
+   
     
     class Egenandel{
         
@@ -23,4 +49,5 @@ public interface RegneBelop {
         
     }
     
-}
+   
+}// end of interface regneBelop.
