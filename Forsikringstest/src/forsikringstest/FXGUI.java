8c58071baf421b4d74859c6c055f05e2caf4f22d
Håkon Sørby <hakonsor@gmail.com>
2015-04-17 16:35:33 +0200

    GUI
    et ret helvete

--- Forsikringstest/src/forsikringstest/FXGUI.java
+++ Forsikringstest/src/forsikringstest/FXGUI.java
@@ -1,4 +1,10 @@
-package sample;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package forsikringstest;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,10 +12,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class Main extends Application {
-
-    @Override
+/**
+ *
+ * @author hakon_000
+ */
+public class FXGUI extends Application {
+    
+    public FXGUI(String[] args){
+        launch(args);
+    }
+    
+        @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
@@ -21,11 +34,5 @@ public class Main extends Application {
         primaryStage.setTitle("Logg inn");
         primaryStage.setScene(new Scene(root, 800, 640));
         primaryStage.show();
-    }
-
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
--- Forsikringstest/src/forsikringstest/Forsikringstest.java
+++ Forsikringstest/src/forsikringstest/Forsikringstest.java
@@ -5,17 +5,40 @@
  */
 package forsikringstest;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Therese
  */
-public class Forsikringstest {
+public class Forsikringstest extends Application{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    //   Register r = new Register();
+        launch(args);
+    }
+    
+        @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setMaxHeight(640);
+        primaryStage.setMinHeight(640);
+        primaryStage.setMaxWidth(800);
+        primaryStage.setMinWidth(800);
+
+        primaryStage.setTitle("Logg inn");
+        primaryStage.setScene(new Scene(root, 800, 640));
+        primaryStage.show();
     }
     
 }
