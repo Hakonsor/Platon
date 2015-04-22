80b25d5fa4c782b44b57a56031be23c47f543a67
Barnholt <Magnus@dhcp-158-39-199-83.wlan.hioa.no>
2015-04-22 16:52:26 +0200

    GUI BIl

--- src/GUI/KundesideBil.java
+++ src/GUI/KundesideBil.java
@@ -5,6 +5,8 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+
+import static javafx.geometry.Pos.CENTER;
 import static javafx.geometry.Pos.TOP_CENTER;
 
 /**
@@ -13,15 +15,123 @@ import static javafx.geometry.Pos.TOP_CENTER;
 public class KundesideBil {
 
     public static Pane bilFane() {
+<<<<<<< Updated upstream
+=======
+        //Group root = new Group();
+        BorderPane borderPane = new BorderPane();
+        HBox vb = new HBox();
+        vb.setPadding(new Insets(25, 25, 25, 25));
+        vb.setSpacing(100);
+        vb.setAlignment(CENTER);
+
+        Label overskrift = new Label();
+        overskrift.setText("Her registrerer du bilforsikring");
+        overskrift.setId("overskrift");
+        vb.getChildren().addAll(overskrift);
+
+        borderPane.setTop(vb);
+
+>>>>>>> Stashed changes
         GridPane grid = new GridPane();
         grid.setAlignment(TOP_CENTER);
         grid.setHgap(10);
+<<<<<<< Updated upstream
         grid.setVgap(10);
         grid.setPadding(new Insets(100));
 
         Label lb = new Label();
         lb.setText("Her kommer det Informasjon");
         GridPane.setHalignment(lb, HPos.CENTER);
+=======
+        grid.setPrefHeight(50);
+        grid.setPrefWidth(800);
+        grid.setAlignment(TOP_CENTER);
+
+
+        TextField tfRegnr = new TextField();
+        tfRegnr.setPromptText("Reg.Nr");
+        tfRegnr.setMinWidth(200);
+
+        TextField tfÅrsmodell = new TextField();
+        tfÅrsmodell.setPromptText("Årsmodell");
+        tfÅrsmodell.setMinWidth(200);
+
+        TextField tfBiltype = new TextField();
+        tfBiltype.setPromptText("Biltype eks (BMW 3-serie)");
+        tfBiltype.setMinWidth(200);
+
+        TextField tfKmstand = new TextField();
+        tfKmstand.setPromptText("Km-stand");
+        tfKmstand.setMinWidth(200);
+
+        Label regLabel = new Label();
+        regLabel.setText("");
+        regLabel.setId("regLabel");
+        regLabel.setMinWidth(300);
+
+        ComboBox<String> cbKjørelengde = new ComboBox<String>();
+        cbKjørelengde.setEditable(false);
+        cbKjørelengde.setMinWidth(200);
+        cbKjørelengde.getItems().addAll(
+                "Kjørelengde: 8 000 km",
+                "Kjørelengde: 15 000 km",
+                "Kjørelengde: 20 000 km",
+                "Kjørelengde: 30 000 km",
+                "Kjørelengde: Ubegrenset"
+        );
+        cbKjørelengde.setValue("Velg Kjørelengde:");
+
+        ComboBox<String> cbBonus = new ComboBox<String>();
+        cbBonus.setEditable(false);
+        cbBonus.setMinWidth(200);
+        cbBonus.getItems().addAll(
+                "Bonus: -20%",
+                "Bonus: -10%",
+                "Bonus: 0%",
+                "Bonus: 10%",
+                "Bonus: 20%",
+                "Bonus: 30%",
+                "Bonus: 40%",
+                "Bonus: 50%",
+                "Bonus: 60%",
+                "Bonus: 70%",
+                "Bonus: 75%",
+                "Bonus: 75% + 1år",
+                "Bonus: 75% + 2år",
+                "Bonus: 75% + 3år",
+                "Bonus: 75% + 4år",
+                "Bonus: 75% + 5år"
+        );
+        cbBonus.setValue("Velg Bonus:");
+
+        ComboBox<String> cbEgenandel = new ComboBox<String>();
+        cbEgenandel.setEditable(false);
+        cbEgenandel.setMinWidth(200);
+        cbEgenandel.getItems().addAll(
+                "Egenandel:  5 000,-",
+                "Egenandel:  7 000,-",
+                "Egenandel: 10 000,-",
+                "Egenandel: 15 000,-"
+        );
+        cbEgenandel.setValue("Velg Egenandel:");
+
+        Button btnRegBilforsikring = new Button();
+        btnRegBilforsikring.setText("Registrer Bilforsikring");
+        btnRegBilforsikring.setId("btnRegBilforsikring");
+        btnRegBilforsikring.setMinWidth(200);
+        btnRegBilforsikring.setOnAction(e -> {
+            System.out.println("Venter på en KODE!!! :D");
+            regLabel.setText("Bilforsikring Registrert!");
+        });
+
+        grid.add(tfRegnr, 0, 0); grid.add(cbKjørelengde, 1, 0);
+        grid.add(tfÅrsmodell, 0, 1); grid.add(cbBonus, 1, 1);
+        grid.add(tfBiltype, 0, 2); grid.add(cbEgenandel, 1, 2);
+        grid.add(tfKmstand, 0, 3); grid.add(btnRegBilforsikring, 1, 3);
+        grid.add(regLabel, 0, 4);
+
+
+>>>>>>> Stashed changes
 
         grid.add(lb, 0, 0);
 
