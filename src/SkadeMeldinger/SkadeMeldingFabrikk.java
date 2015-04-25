b4d26d776e892b11c4418e138f038e6cccffde80
TheresePlaton <s929511@hioa.no>
2015-04-25 19:35:06 +0200

    laget skademeldingsfabrikk som lager skademeldingene. Lagt til tomme konstruktører slik at vi kan lage skademeldinger og legge til info etterhvert som brukerne sriver det inn. Forsikringen legges til ved opprettelse av skademeldingen

---
+++ src/SkadeMeldinger/SkadeMeldingFabrikk.java
@@ -0,0 +1,63 @@
+package SkadeMeldinger;
+
+
+import Forsikring.BatForsikring;
+import Forsikring.BilForsikring;
+import Forsikring.BoligForsikring;
+import Forsikring.Forsikringer;
+import Forsikring.ReiseForsikring;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Therese
+ */
+
+  
+public class SkadeMeldingFabrikk implements Serializable{ 
+   
+    public SkadeMeldingFabrikk(){
+       
+    }
+ // lager skademeldinger klar til å motta 
+//informasjon og registrerer Forsikringen som gjelder i skademeldingen
+    public static SkadeMelding lagSkadeMelding( Forsikringer fors ){
+        if( verifiser ( fors ) ){
+            SkadeMelding skade;
+            skade = null;
+            if( fors instanceof BilForsikring ){
+                skade = new BilSkadeMelding(); 
+                
+            }
+            else if( fors instanceof BatForsikring ){
+                skade = new BatSkadeMelding();
+                
+            }
+            else if( fors instanceof BoligForsikring ){
+                skade =  new BoligSkadeMelding();
+                
+            }
+            else if( fors instanceof ReiseForsikring ){
+                skade = new PersonSkadeMelding();
+                
+            }
+            if( skade != null ){
+                skade.setForsikring( fors );
+            }
+            return skade;
+       }
+        return null;
+        
+    }// end of method lagSkadeMelding
+    
+    // skal returnere true dersom forsikringen finnes, hvis ikke false
+    // skademeldingen kan dermed ikke opprettes.
+    public static  boolean verifiser(Forsikringer fors){
+        
+        // metoden er under bygging
+        return true;
+       
+    }
+    
+}// end of class SkadeMeldingFabrikk
+
