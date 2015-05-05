8484e756bee30b2e419d4c422a0b92e808c17320
TheresePlaton <s929511@hioa.no>
2015-05-05 02:29:39 +0200

    Merge origin/master
    Conflicts:
    	src/GUI/KonsulentsideSkade.java

--- src/GUI/KundesideBåt.java
+++ src/GUI/KundesideBåt.java
@@ -3,6 +3,7 @@ package GUI;
 import Kontroller.ComboBoxConverter;
 import Kontroller.Kontroller;
 import Person.Person;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
