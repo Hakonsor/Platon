e0d41da28fba2d904df56e1f454db8bb62d8fe42
TheresePlaton <s929511@hioa.no>
2015-05-19 14:07:57 +0200

    en liten endring i melding

--- src/SkadeMeldinger/BatSkadeMelding.java
+++ src/SkadeMeldinger/BatSkadeMelding.java
@@ -5,35 +5,40 @@
  */
 package SkadeMeldinger;
 
+import Forsikring.BatForsikring;
+import Forsikring.BilForsikring;
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.Calendar;
 
 /**
- * Klassen skal representere det som er unikt for en båtskademelding, så langt er det 
- * bare en toStringmetode, men kan utvides til å inneholde mer. Funksjonen klassen har nå er at
- * man kan bruke polymorfisme til å kjenne den igjen og instansiere.
+ * Klassen skal representere det som er unikt for en båtskademelding, så langt
+ * er det bare en toStringmetode, men kan utvides til å inneholde mer.
+ * Funksjonen klassen har nå er at man kan bruke polymorfisme til å kjenne den
+ * igjen og instansiere.
  *
  * @author Therese
  */
-public class BatSkadeMelding extends SkadeMelding implements Serializable{
-   
-    
+public class BatSkadeMelding extends SkadeMelding implements Serializable {
+
     public BatSkadeMelding(String skadeBeskrivelse, int utbetal, Calendar skadeDato) {
         super(skadeBeskrivelse, utbetal, skadeDato);
     }
-    
-    
+
     // teksten som skal vises i vinduet når skademeldingen er registrert.
-    public String melding(){
+    public String melding() {
+
+        BatForsikring f = (BatForsikring) super.getForsikring();
+        String form = "0.00";
+        DecimalFormat tall = new DecimalFormat(form);
         String s;
-        s = "Båtskademelding \nInnmeldt av: " +  super.getForsikring().getKunde().getFornavn()
-                +" "+super.getForsikring().getKunde().getEtternavn()+"\n"+
-                
-                super.toString();
-        s+= "Premie før skade: " + super.getForsikring().getPremie() + " kr" + 
-             "\nPremie etter skade: " + super.getForsikring().getPremieTilGodkjenning() + " kr";
-        
+        s = "Båtskademelding \nInnmeldt av: " + f.getKunde().getFornavn()
+                + " " + f.getKunde().getEtternavn() + "\n"
+                + super.toString()
+                + "\nPremie før skade: " + tall.format(f.getPremie()) + " kr"
+                + "\nPremie etter skade: " + tall.format(f.getPremieTilGodkjenning()) + " kr";
+
         return s;
     }
-    
+
 }
